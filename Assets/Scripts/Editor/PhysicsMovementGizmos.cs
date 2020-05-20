bbc844187ac3d345545e9559ebf3e71f9f45298c
FireFlyForLife <maiko.s.99@gmail.com>
2020-05-20 15:03:32 +0200

    Initial Unity project

---
+++ Assets/Scripts/Editor/PhysicsMovementGizmos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PhysicsMovementGizmos : EditorWindow
+{
+    [MenuItem("Tools/PhysicsMovementGizmos")]
+    public static void ShowPhysicsMovementGizmos()
+    {
+        var physicsMovementWindow = EditorWindow.GetWindow<PhysicsMovementGizmos>();
+        physicsMovementWindow.Show();
+    }
+
+    // ReSharper disable once InconsistentNaming
+    protected void OnGUI()
+    {
+        EditorGUILayout.LabelField("Hello Physics Movement Gizmos!");
+    }
+}
