c107bb567344198fcec5dd7101670e4998488043
FireFlyForLife <maiko.s.99@gmail.com>
2020-05-20 15:57:31 +0200

    Added drawline forward script

---
+++ Assets/Scripts/DrawArrowForward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+#if UNITY_EDITOR
+using UnityEngine;
+#endif
+
+public class DrawArrowForward : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [DrawGizmo(GizmoType.Selected | GizmoType.NonSelected)]
+    static void DrawGizmosArrow(DrawArrowForward drawArrow, GizmoType type)
+    {
+        Gizmos.DrawLine(drawArrow.transform.position, drawArrow.transform.position + drawArrow.transform.forward * 0.75f);
+        Gizmos.DrawWireSphere(drawArrow.transform.position, 0.05f);
+    }
+#endif
+}
