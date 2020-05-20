bf286b1a692af2c6426e7bdb016a9e594f6a44e6
Marjolein <marjoleinkaal@gmail.com>
2020-05-20 21:16:20 +0200

    Added Gears with scripts to link them to each other

---
+++ Assets/_NewVesta/Scripts/Gears/RotateGear.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGear : MonoBehaviour
+{
+    public float turnSpeed = 50f;
+    public float ToothCount;
+
+    public RotateGearChild child;
+
+    private void Update()
+    {
+        float rotation = 0;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            rotation = turnSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            rotation = -turnSpeed * Time.deltaTime;
+
+        transform.Rotate(transform.forward, rotation);
+
+        child.Rotate(-rotation * ToothCount);
+    }
+}
---
+++ Assets/_NewVesta/Scripts/Gears/RotateGearChild.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGearChild : MonoBehaviour
+{
+    public float ToothCount;
+
+    public List<RotateGearChild> rotateGearChildren;
+
+    public void Rotate(float rotation)
+    {
+        rotation /= ToothCount;
+        transform.Rotate(transform.forward * rotation);
+
+        foreach (RotateGearChild gear in rotateGearChildren)
+        {
+            gear.Rotate(-rotation * ToothCount);
+        }
+    }
+}
