8e581ce1b5451a1bae93fda2bdcd1c7df43f6e89
FireFlyForLife <maiko.s.99@gmail.com>
2020-06-02 18:14:34 +0200

    Added physics based player prefab

---
+++ Assets/_NewVesta/Scripts/PhysicsDirectFollower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsDirectFollower : MonoBehaviour
+{
+    [Tooltip("The rigidbody that will act as a target point for this rigidbody to follow")]
+    public Rigidbody targetRigidbody;
+
+    [Tooltip("The rigidbody that will be moved towards the target rigidbody, If null will be initialized to GetComponent<Rigidbody>()")]
+    public Rigidbody thisRigidbody;
+
+    void Start()
+    {
+        thisRigidbody = thisRigidbody ?? GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 deltaMovement = thisRigidbody.position - targetRigidbody.position;
+        targetRigidbody.velocity = deltaMovement / Time.fixedDeltaTime;
+
+        Quaternion deltaRotation = transform.rotation * Quaternion.Inverse(targetRigidbody.rotation);
+        Vector3 deltaRotationEuler = deltaRotation.eulerAngles;
+        Vector3 eulerRotation = new Vector3(Mathf.DeltaAngle(0, deltaRotationEuler.x), Mathf.DeltaAngle(0, deltaRotationEuler.y), Mathf.DeltaAngle(0, deltaRotationEuler.z));
+        eulerRotation *= 0.95f * Mathf.Deg2Rad;
+        thisRigidbody.angularVelocity = eulerRotation / Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/_NewVesta/Scripts/PlayerActor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PlayerActor : MonoBehaviour
+{
+    public static PlayerActor Instance { get; private set; }
+    
+    [Header("Unity XR Rig")]
+    public GameObject xrRigObject;
+    public GameObject xrCameraOffsetObject;
+    public Camera xrCamera;
+    public XRController xrLeftHand;
+    public XRController xrRightHand;
+
+    [Header("Physics Player")]
+    public Rigidbody headRigidbody;
+    public Rigidbody leftHandRigidbody;
+    public Rigidbody rightHandRigidbody;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogError($"There is more than one PlayerActor in the scene! Disabling {this}");
+            gameObject.SetActive(false);
+        }
+    }
+}
